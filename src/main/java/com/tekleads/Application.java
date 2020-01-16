fcdf5aba9cbf262bc16747f9e59d6e38c67c7e95
vijay <vijay@tcs.com>
2020-01-16 17:42:20 +0530

    Sysout removed

--- src/main/java/com/tekleads/Application.java
+++ src/main/java/com/tekleads/Application.java
@@ -10,7 +10,6 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.println("Eureka Server Enabled");
 		System.out.println("Eureka Server Enabled::::");
 	}
 
